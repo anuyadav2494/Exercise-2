04c538ae0917157a870fab8cb94dd2a2740a93d2
Anu Yadav Kambalapally <anuyadav@usf.edu>
2020-01-21 20:58:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            int age;
+            int height;
+            // Max Heart rate is equal to 220-age
+
+
+            age = 24;
+            height = 155;
+            int maxheartrate;
+            maxheartrate = 220 - age;
+            Console.WriteLine("I am " + age + " years old, my max heart rate is  " + maxheartrate + " and I am " + height + " cms tall.");
+            Console.ReadLine();
+        }
+    }
+}
